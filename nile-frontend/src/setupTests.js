be4fa89af68d9b489cca202d173cc46b724161c2
mohamedsorour1998 <mohamedsorour1998@gmail.com>
2026-05-14 15:04:36 +0300

    fix: jest moduleNameMapper and TextEncoder polyfill for react-router-dom v7

--- nile-frontend/src/setupTests.js
+++ nile-frontend/src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Polyfills required by react-router v7 under Jest 27
+const { TextEncoder, TextDecoder } = require('util');
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
