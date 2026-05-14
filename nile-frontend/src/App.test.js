1c43ea0c0881f535549c4903961bf2c226a50ddd
mohamedsorour1998 <mohamedsorour1998@gmail.com>
2026-05-14 15:02:16 +0300

    test: add Home smoke test (failing)

--- nile-frontend/src/App.test.js
+++ nile-frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './components/Home';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Home renders welcome heading', () => {
+  render(<BrowserRouter><Home /></BrowserRouter>);
+  expect(screen.getByText(/Welcome to Nile/i)).toBeInTheDocument();
 });
