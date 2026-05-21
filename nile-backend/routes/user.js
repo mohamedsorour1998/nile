cf90d82437f2ad94e0b4e05cb1654bc4e87acb2d
mohamedsorour1998 <mohamedsorour1998@gmail.com>
2026-05-21 11:52:06 +0300

    feat: add follow and changePassword routes

--- nile-backend/routes/user.js
+++ nile-backend/routes/user.js
@@ -1,9 +1,11 @@
-const express                 = require('express');
-const { getUser, updateUser } = require('../controllers/user');
-const requireAuth             = require('../middleware/auth');
-const router                  = express.Router();
+const express                                        = require('express');
+const { getUser, updateUser, followUser, changePassword } = require('../controllers/user');
+const requireAuth                                    = require('../middleware/auth');
+const router                                         = express.Router();
 
-router.get('/:id',  requireAuth, getUser);
-router.put('/:id',  requireAuth, updateUser);
+router.get('/:id',           requireAuth, getUser);
+router.put('/:id',           requireAuth, updateUser);
+router.post('/:id/follow',   requireAuth, followUser);
+router.put('/:id/password',  requireAuth, changePassword);
 
 module.exports = router;
